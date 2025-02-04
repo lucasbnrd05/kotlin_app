ecb989f101d340c27db697fe4277f91186ebfeeb
Lucas Bernardeau <lucas.bernardeau@epita.fr>
2025-02-04 21:31:58 +0100

    hello_word

--- ToilExplore/app/src/main/java/com/example/toilexplore/MainActivity.kt
+++ ToilExplore/app/src/main/java/com/example/toilexplore/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import androidx.appcompat.widget.Toolbar
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Configurer la Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
