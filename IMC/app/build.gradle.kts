b897e5a9465d7e9bbf44cf1af0b8b90f0280bba7
Lucas Bernardeau <lucas.bernardeau@epita.fr>
2025-02-25 17:12:01 +0100

    button

--- IMC/app/build.gradle.kts
+++ IMC/app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.osmdroid.android)
+    implementation(libs.androidx.constraintlayout)
 }
